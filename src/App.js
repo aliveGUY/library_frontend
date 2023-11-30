4a18a9d86373646e29d78375128360fce2edbad9
Illia Dubrovin <ilyadubrivvin@gmail.com>
2023-12-01 01:59:56 +0200

    release (#4)
    * enit basic react and pages
    
    * Problem with conect
    
    * fix db conn
    
    * refactor front end
    
    ---------
    
    Co-authored-by: unknown <askorbinkadevochka@gmail.com>
    Co-authored-by: rocksoul666 <91737451+rocksoul666@users.noreply.github.com>
    Co-authored-by: illdub <illia.dubrovin@dreamhost.com>

--- src/App.js
+++ src/App.js
@@ -1,37 +1,23 @@
-import { Routes, Route } from "react-router-dom";
-import Layout from "./components/Layout";
-import Public from "./components/Public";
-import Login from "./features/auth/login";
-import DashLayout from "./components/DashLayout";
-import Welcome from "./features/auth/Welcome";
-import NotesList from "./features/notes/NotesList";
-import UsersList from "./features/users/UsersList";
-import AuthorsList from "./features/authors/AuthorsList";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Public />} />
-
-        <Route path="authors" element={<AuthorsList />} />
-
-        <Route path="login" element={<Login />} />
-
-        <Route path="dash" element={<DashLayout />}>
-          <Route index element={<Welcome />} />
-
-          <Route path="notes">
-            <Route index element={<NotesList />} />
-          </Route>
-
-          <Route path="users">
-            <Route index element={<UsersList />} />
-          </Route>
-        </Route>
-      </Route>
-
-    </Routes>
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <div className="pages-wrapper">
+          <div className="pages">
+            <Routes>
+              <Route path="/" element={<Home />} />
+            </Routes>
+          </div>
+        </div>
+        <Footer />
+      </BrowserRouter>
+    </div>
   );
 }
 
--- src/app/api/apiSlice.js
+++
@@ -1,7 +0,0 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-
-export const apiSlice = createApi({
-    baseQuery: fetchBaseQuery({ baseUrl: 'https://library-uni-project-api.onrender.com' }),
-    tagTypes: ['Book', 'Order', 'User'],
-    endpoints: builder => ({})
-})

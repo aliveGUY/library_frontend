52e9ebe54e2acfc9158d838d95953538723fd627
illdub <illia.dubrovin@dreamhost.com>
2023-12-04 23:58:31 +0200

    add render.yaml

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import App from './App';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
+
+if (process.env.NODE_ENV === 'production') disableReactDevTools()
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,7 +15,7 @@ root.render(
     <Provider store={store}>
       <BrowserRouter>
         <Routes>
-          <Route path='/*' element={<App />} />
+          <Route path="/*" element={<App />} />
         </Routes>
       </BrowserRouter>
     </Provider>
